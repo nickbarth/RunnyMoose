6a96e0f80aeef473cc6fc0e397ee77d6a2656851
nickbarth <nickbarth@live.com>
2014-10-16 22:47:56 -0600

    centered runny moose

--- source/Player.hx
+++ source/Player.hx
@@ -9,16 +9,19 @@ import flixel.util.FlxAngle;
 import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
 
+using flixel.util.FlxSpriteUtil;
+
 class Player extends FlxSprite
 {
   public function new(X:Float=0, Y:Float=0)
   {
     super(X, Y);
 
-    x = 0;
+    screenCenter();
     loadGraphic("assets/images/moose_run.png", true, 60, 64);
     animation.add("run", [0, 1], 20, true);
     animation.play("run");
+    x = 0;
   }
 
   private function movement():Void
